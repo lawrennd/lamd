8db8326f24afe2fde9da301cd28fe67fbc56a0f8
Neil Lawrence <lawrennd@gmail.com>
2018-07-11 02:26:16 +0100

    Update papers and layouts.

--- mkcv.sh
+++ mkcv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd parts
-pandoc header.md currentEmployment.md policy.md communication.md healthInternational.md industrial.md public.md communityLeadership.md otherProfessional.md previousEmployment.md education.md -o ../cv.docx
+cd includes
+pandoc header.md current_employment.md policy.md communication.md health_international.md industrial.md public.md community_leadership.md other_professional.md previous_employment.md education.md -o ../cv.docx
 cd ..
