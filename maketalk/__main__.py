fe0d56854ee1889661bc51f5255182f7439ae928
Neil Lawrence <lawrennd@gmail.com>
2021-04-11 17:30:05 +0100

    Update for executable scripts.

--- maketalk/__main__.py
+++
@@ -1,1 +0,0 @@
-from flags import main as flags
--- maketalk/dependencies.py
+++ maketalk/dependencies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-import _python.ndltalk as nt
+import ndlpy.talk as nt
 
 def main(args=None):
     parser = argparse.ArgumentParser()
