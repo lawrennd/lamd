23302e95211236db0e781edb57380879d95d7a74
Neil Lawrence <lawrennd@gmail.com>
2021-04-14 13:04:34 +0100

    Update mdpp

---
+++ makemd/mdlist.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import argparse
+
+import frontmatter as fm
+
+import ndlpy.talk as nt
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("output",
+                        type=str,
+                        choices=['talks', 'grants', 'extalks', 'teaching', 'students', 'exstudents', 'pdras', 'expdras'],
+                        help="The type of output markdown list")
+
+    parser.add_argument('file', type=argparse.FileType('r'), nargs='+',
+                        help="The file names to read in")
+    
+    args = parser.parse_args()
+
+
+    entries = []
+    for file in args.file:
+        name, ext = os.path.splitext(file.name)
+        if ext == 'yaml':
+            metadata, _ = fm.parse(file.read())
+            entries.append(metadata)
+        elif ext = 'csv':
+            csv_entries = csv.DictReader(file)
+            entries += csv_entries
+
+    print(entries)
+
+
+        
+    #fields = ny.header_fields(filename)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
